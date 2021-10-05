9d40369fb6b818a6c7c5a50dc8983e820aed82c5
Diejo612 <90541272+Diejo612@users.noreply.github.com>
2021-10-04 20:56:18 -0500

    Se añadieron las funciones para generar un artista aleatorio y canción del artista. Además de función para mostrar mensaje aleatorio.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // generate random number
 const randInd = n => Math.floor(Math.random()*n);
 
-// create an object with music to recommend [artist-music]
+// create an object with music to recommend [artist-songs]
 
 const music = {
     'honne' : ['free love', 'Me & You', 'Warm on a Cold Night'],
@@ -12,3 +12,25 @@ const music = {
     'Siddhartha' : ['Únicos', 'A la Distancia', 'Ecos de Miel', 'Extraños', 'Algún día (Cap. 1)', 'Me Hace Falta (Cap. 2)', 'Buscándote (Cap.7)']
 };
 
+// function for select random artist and song
+
+const randArtist = () => {
+    const artists = Object.keys(music);
+    return artists[randInd(artists.length)];
+};
+
+const randSong = (artist) => {
+    const songs = music[artist];
+    return songs[randInd(songs.length)];
+};
+
+// function for show random message
+
+const showMsg = () => {
+    const artist = randArtist(music);
+    const song = randSong(artist);
+
+    return `Recommended song of the day is: \n${song} by ${artist}.\nEnjoy it! :)`
+}; 
+
+console.log(showMsg());
