d9af88a41c6246679342ed6d11b436658b016027
Diejo612 <90541272+Diejo612@users.noreply.github.com>
2021-10-04 20:29:20 -0500

    Añadir archivo script.js y ffunción para generar índice aleatorio. Además se añadió objeto con música para mostrar.

---
+++ script.js
@@ -0,0 +1,14 @@
+// generate random number
+const randInd = n => Math.floor(Math.random()*n);
+
+// create an object with music to recommend [artist-music]
+
+const music = {
+    'honne' : ['free love', 'Me & You', 'Warm on a Cold Night'],
+    'Keane' : ['Bedshaped', 'This Is The Last Time', 'Somewhere Only we know', 'Everybody\'s Changing', 'Love too Much', 'Disconnected'],
+    'Harry Styles' : ['Girl Crush', 'Adore You', 'Falling', 'Sign of The Times', 'Golden', 'Watermelon Sugar'],
+    'Bad Bunny': ['Amorfoda', 'Dakiti', 'Yo Perreo Sola'],
+    'Paté de Fuá': ['Háblame de ti', 'Cien años', 'Llevame en un Beso'],
+    'Siddhartha' : ['Únicos', 'A la Distancia', 'Ecos de Miel', 'Extraños', 'Algún día (Cap. 1)', 'Me Hace Falta (Cap. 2)', 'Buscándote (Cap.7)']
+};
+
